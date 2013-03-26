7e41a29a9071549050a7c044e4c2b3234dba0649
ios3401 <ios3401@caelum57-17.local>
2013-03-26 19:15:08 -0300

    Initial Commit

---
+++ ContatoIP67/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContatoIP67
+//
+//  Created by ios3401 on 26/03/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContatoIP67/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContatoIP67
+//
+//  Created by ios3401 on 26/03/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
