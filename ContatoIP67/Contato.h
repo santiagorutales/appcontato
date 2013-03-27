84faa78d01d55c683fe5f6b797eed166c7bead1e
ios3401 <ios3401@caelum57-17.local>
2013-03-26 21:57:15 -0300

    Contatos v0.1

---
+++ ContatoIP67/Contato.h
@@ -0,0 +1,13 @@
+//
+//  Contato.h
+//  ContatoIP67
+//
+//  Created by ios3401 on 26/03/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contato : NSObject
+
+@end
---
+++ ContatoIP67/Contato.m
@@ -0,0 +1,13 @@
+//
+//  Contato.m
+//  ContatoIP67
+//
+//  Created by ios3401 on 26/03/13.
+//  Copyright (c) 2013 br.com.caelum. All rights reserved.
+//
+
+#import "Contato.h"
+
+@implementation Contato
+
+@end
